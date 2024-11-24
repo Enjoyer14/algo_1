9bf1c9fbe65d4feb9624ba3ba2cecb9004952f8f
stas <stassah22@gmail.com>
2024-11-24 13:21:44 +0300

    Выполнение ДЗ по алгоритмам

--- main.nim
+++ main.nim
@@ -1,6 +1,25 @@
 # импортируйте нужные библиотеки (советуем strutils и sequtils)
+import strutils, sequtils
 
 proc main() =
-  # Реализуйте ваш код здесь
+  let n = stdin.readLine.parseInt
+  let points = stdin.readLine.split(" ").mapIt(it.parseInt)
+  let k = stdin.readLine.parseInt
+
+  var left = 0
+  var right = points.high
+  var summa = 0
+
+  while(left < right):
+    summa = points[left] + points[right]
+    if(summa == k):
+      stdout.writeLine(points[left], " ", points[right])
+      return
+    elif(summa > k):
+      right -= 1
+    else:
+      left += 1
+  stdout.writeLine("None")
+
 
 main()
